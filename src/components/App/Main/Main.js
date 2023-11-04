701d8214e6c1be9ab7eb2ab3b801d36c0502da9b
Sarah-Emidio <o.sarah.emidio@gmail.com>
2023-11-04 16:46:56 +0100

    integration du menu

--- src/components/App/Main/Main.js
+++ src/components/App/Main/Main.js
@@ -1,9 +1,40 @@
 import '../Main/Main.scss';
 
 const Main = () => {
-    return(
-        <div>
-            <h2>Menu</h2>
+    return (
+        <div className='main__container'>
+
+            <div className='main__header'>
+                <span className='main__title'><span className='main__logo'>C</span>
+                    Ciel bleu
+                </span>
+
+            </div>
+
+            <div className='main__border'></div>
+
+            <div className='main_buttonContainer'>
+                <button className='main__button' type="button">
+                    + Nouvelle mission
+                </button>
+            </div>
+
+
+            <nav className='main__linkContent'>
+                <ul>
+                    <li className='main__link'>Tableau de bord</li>
+                    <li className='main__link'>Planning</li>
+                    <li className='main__linkColor'>Missions</li>
+                    <li className='main__link'>Factures</li>
+                </ul>
+
+                <ul>
+                    <li className='main__link'>Paramètres</li>
+                    <li className='main__link'>Aide</li>
+                    <li className='main__link'>Factures</li>
+                </ul>
+            </nav>
+
         </div>
     )
 }
