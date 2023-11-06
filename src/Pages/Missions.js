2b074b0d7cb3b2e4a0d4e3c546d767ba9ebc3aad
Sarah-Emidio <o.sarah.emidio@gmail.com>
2023-11-06 14:19:43 +0100

    ajout de la fonction du menu cliquable

--- src/Pages/Missions.js
+++ src/Pages/Missions.js
@@ -1,14 +1,42 @@
+import { useEffect, useState } from "react";
 import Main from "../components/App/Main/Main";
 import Header from "../components/App/Header/Header";
 
 import '../Pages/Missions.scss';
 
 const Missions = () => {
+    const [showMain, setShowMain] = useState(false);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (windowWidth <= 320) {
+            setShowMain(false);
+        } else {
+            setShowMain(true);
+        }
+    }, [windowWidth]);
+
+    const toggleMain = () => {
+        setShowMain(!showMain);
+    }
+
     return (
         <>
             <div className="container">
-                <Main />
-                <Header />
+                {showMain && <Main />}
+                <Header toggleMain={toggleMain}/>
             </div>
         </>
     )
