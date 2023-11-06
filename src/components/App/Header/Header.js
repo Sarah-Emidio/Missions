154e6a45e2ce6f5ce10023852370087bc74d28fb
Sarah-Emidio <o.sarah.emidio@gmail.com>
2023-11-06 14:19:07 +0100

    ajout de la fonction du menu cliquable

--- src/components/App/Header/Header.js
+++ src/components/App/Header/Header.js
@@ -2,7 +2,7 @@ import menu from '../../../assets/menu.svg';
 import '../Header/Header.scss';
 
 
-const Header = () => {
+const Header = ({toggleMain}) => {
     return (
         <>
             <header className='header__content'>
@@ -13,19 +13,22 @@ const Header = () => {
                         <span className='header__subtitle'>7</span>
                     </h1>
 
-                    <button className='header__button' type="submit">
+                    <button className='header__button' type="submit" onClick={toggleMain}>
                         <img className='header__img' src={menu} alt="" />
                     </button>
-               
+
                 </div>
 
                 <nav >
                     <ul className='nav__container'>
-                        <li>A pourvoir</li>
-                        <li>En cours</li>
-                        <li>A venir</li>
-                        <li>Terminées</li>
-                        <li>Annulées</li>
+                        <div className='nav__linkOn'>
+                            <li className='nav__link'>A pourvoir</li>
+                        </div>
+
+                        <li className='nav__link'>En cours</li>
+                        <li className='nav__link'>A venir</li>
+                        <li className='nav__link'>Terminées</li>
+                        <li className='nav__link'>Annulées</li>
                     </ul>
                 </nav>
 
