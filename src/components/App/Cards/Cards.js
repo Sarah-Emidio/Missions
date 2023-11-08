b24ccd6ee198fbb857725c44297723fd7cab3b1a
Sarah-Emidio <o.sarah.emidio@gmail.com>
2023-11-08 16:52:53 +0100

    début ajout de style aux cards

--- src/components/App/Cards/Cards.js
+++ src/components/App/Cards/Cards.js
@@ -3,23 +3,23 @@ import Candidat from '../../../assets/Candidat.png';
 
 
 const Cards = () => {
-    return(
+    return (
         <div className="card">
-        <div className="card__row">
-            <div className="card__header"><input type="checkbox" />Table header</div>
-            <div className="card__header">Tableau Header</div>
-            <div className="card__header">Tableau Header</div>
-            <div className="card__header">Tableau Header</div>
-            <div className="card__header">Candidat</div>
+            <div className="card__head">
+                <div className='card__head__metier'>
+                    <p>Métiers <span className='card__head__price'>00,00€</span></p>
+                </div>
+
+                <div className='card__head__date'>
+                    <p>Date <span>Badge</span><span></span></p>
+                </div>
+            </div>
+
+            <div className="card__body">
+                <img className="card__body__img" src={Candidat} alt="" />
+                <p className='card___body__link'>Voir les Candidatures</p>
+            </div>
         </div>
-        <div className="card__row">
-            <div className="card__cell"><input type="checkbox" />Table Cell</div>
-            <div className="card__cell">Text<span className="badge"> Badge</span></div>
-            <div className="card__cell">Table Cell 2</div>
-            <div className="card__cell">Table Cell 3</div>
-            <div className="card__cell"><img className="card__img" src={Candidat} alt=""  /></div>
-        </div>
-    </div>
     )
 }
 
