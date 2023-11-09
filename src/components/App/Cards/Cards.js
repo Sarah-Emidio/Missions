62e710de9f3bd7281a7050809dbed3686a353ffa
Sarah-Emidio <o.sarah.emidio@gmail.com>
2023-11-09 11:15:46 +0100

    integration du style est des positions des cards en smalldevices

--- src/components/App/Cards/Cards.js
+++ src/components/App/Cards/Cards.js
@@ -1,23 +1,27 @@
 import '../Cards/Cards.scss';
 import Candidat from '../../../assets/Candidat.png';
 
-
 const Cards = () => {
     return (
         <div className="card">
             <div className="card__head">
-                <div className='card__head__metier'>
-                    <p>Métiers <span className='card__head__price'>00,00€</span></p>
+                <div className='card__head__metierContent'>
+                   
+                    <p className='card__head__metier'>Métiers</p>
+                    <span className='card__head__price'>00,00€</span>
+               
                 </div>
+                <p className='card__head__badge'>Badge</p>
 
-                <div className='card__head__date'>
-                    <p>Date <span>Badge</span><span></span></p>
+                <div className='card__head__dateContent'>
+                    <p className='card__head__date'> Date</p>
+                    <p className='card__head__hour'>horaire</p>
                 </div>
             </div>
 
             <div className="card__body">
                 <img className="card__body__img" src={Candidat} alt="" />
-                <p className='card___body__link'>Voir les Candidatures</p>
+                <p className='card__body__link'>Voir les Candidatures</p>
             </div>
         </div>
     )
